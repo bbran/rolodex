f69a624cf40516f9d7e23b7873d6da071caddac0
Peter Boudreau <Peter.Boudreau@LibertyMutual.com>
2017-09-05 18:35:32 -0400

    adding swagger details, mean-bean, getters/setters for the phone and address entities

--- src/main/java/com/libertymutual/goforcode/rolodex/models/Address.java
+++ src/main/java/com/libertymutual/goforcode/rolodex/models/Address.java
@@ -26,5 +26,53 @@ public class Address {
 	
 	@Column(nullable=true)
 	private int zipCode;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
 	
 }
--- src/main/java/com/libertymutual/goforcode/rolodex/models/PhoneNumber.java
+++ src/main/java/com/libertymutual/goforcode/rolodex/models/PhoneNumber.java
@@ -17,6 +17,30 @@ public class PhoneNumber {
 	
 	@Column(nullable=false)
 	private Long phoneNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 	
 
 }
