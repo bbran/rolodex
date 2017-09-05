190be1d93789323eda139dbeff240feb95caf083
Ben Brandvig <benjamin.brandvig@libertymutual.com>
2017-09-05 14:59:41 -0700

    Added card entity and repo

---
+++ src/main/java/com/libertymutual/goforcode/rolodex/models/Card.java
@@ -0,0 +1,74 @@
+package com.libertymutual.goforcode.rolodex.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Card {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String firstName;
+	
+	@Column(nullable=false)
+	private String lastName;
+	
+	@Column(nullable=false)
+	private String title;
+	
+	@Column
+	private String companyName;
+	
+	//private List<Address> addresses;
+	
+	//private List<PhoneNumber> phoneNumbers;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+}
