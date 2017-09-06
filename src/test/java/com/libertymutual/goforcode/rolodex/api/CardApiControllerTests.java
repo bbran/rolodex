d9bb1a689e68decca6ace45b6698028bfd6b5ad7
Ben Brandvig <benjamin.brandvig@libertymutual.com>
2017-09-06 15:57:15 -0700

    added controller tests

--- src/test/java/com/libertymutual/goforcode/rolodex/api/CardApiControllerTests.java
+++ src/test/java/com/libertymutual/goforcode/rolodex/api/CardApiControllerTests.java
@@ -1,26 +1,31 @@
 package com.libertymutual.goforcode.rolodex.api;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.junit.Before;
 import com.libertymutual.goforcode.rolodex.models.Card;
+import com.libertymutual.goforcode.rolodex.repositories.AddressRepo;
 import com.libertymutual.goforcode.rolodex.repositories.CardRepo;
+import com.libertymutual.goforcode.rolodex.repositories.PhoneNumberRepo;
 
 
 public class CardApiControllerTests {
 	
 	private CardRepo cardRepo;
+	private AddressRepo addressRepo;
+	private PhoneNumberRepo	phoneNumberRepo;
 	private CardApiController controller;
 	
 	@Before
 	public void setUp() {
 		cardRepo = mock(CardRepo.class);
-//		controller = new CardApiController(cardRepo);
+		addressRepo = mock(AddressRepo.class);
+		phoneNumberRepo = mock(PhoneNumberRepo.class);
+		controller = new CardApiController(cardRepo, addressRepo, phoneNumberRepo);
 	}
 	
 	
@@ -55,6 +60,49 @@ public class CardApiControllerTests {
 		//assert
 		assertThat(actual).isEqualTo(randomCard);
 		verify(cardRepo).findOne(4l);
+	}
+	
+	@Test
+	public void test_createNew_creates_and_returns_new_card()	{
+		//arrange
+		Card card = new Card();
+		when(cardRepo.save(card)).thenReturn(card);
+		
+		//act
+		Card cardReturned = controller.createNew(card);
+		
+		//assert
+		assertThat(cardReturned).isSameAs(card);
+		verify(cardRepo).save(card);
+	}
+	
+	@Test
+	public void test_delete_returns_card_that_was_deleted()	throws EmptyResultDataAccessException {
+		//arrange
+		Card card = new Card();
+		when(cardRepo.findOne(1L)).thenReturn(card);
+		
+		//act
+		Card cardReturned = controller.delete(1L);
+		
+		//assert
+		assertThat(cardReturned).isSameAs(card);
+		verify(cardRepo).findOne(1L);
+		verify(cardRepo).delete(1L);
+	}
+	
+	@Test
+	public void test_delete_returns_EmptyResultDataAccessException_when_card_not_found()	{
+		//arrange
+		when(cardRepo.findOne(1L)).thenThrow(new EmptyResultDataAccessException(0));
+		
+		//act
+		Card cardReturned = controller.delete(1L);
+		
+		//assert
+		assertThat(cardReturned).isNull();
+		verify(cardRepo).findOne(1L);
+		verify(cardRepo).delete(1L);
 	}
 	
 	
--- src/test/java/com/libertymutual/goforcode/rolodex/models/AddressTests.java
+++ src/test/java/com/libertymutual/goforcode/rolodex/models/AddressTests.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class AddressTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
