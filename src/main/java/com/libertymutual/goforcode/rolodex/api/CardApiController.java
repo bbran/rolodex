638f62150d6c45164ffd75dd5f3bc06dcff09d46
Peter Boudreau <Peter.Boudreau@LibertyMutual.com>
2017-09-06 16:39:03 -0400

    commit changes that update the controller with the right file and ben's changes for test data, and add this. statements

--- src/main/java/com/libertymutual/goforcode/rolodex/api/CardApiController.java
+++ src/main/java/com/libertymutual/goforcode/rolodex/api/CardApiController.java
@@ -11,22 +11,44 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.libertymutual.goforcode.rolodex.models.Address;
 import com.libertymutual.goforcode.rolodex.models.Card;
+import com.libertymutual.goforcode.rolodex.models.PhoneNumber;
+import com.libertymutual.goforcode.rolodex.repositories.AddressRepo;
 import com.libertymutual.goforcode.rolodex.repositories.CardRepo;
+import com.libertymutual.goforcode.rolodex.repositories.PhoneNumberRepo;
 
 @RestController
 @RequestMapping("/api/cards")
 public class CardApiController {
 	private CardRepo cardRepo;
+	private AddressRepo addressRepo;
+	private PhoneNumberRepo phoneNumberRepo;
 	
-	public CardApiController(CardRepo cardRepo)	{
+	public CardApiController(CardRepo cardRepo, AddressRepo addressRepo, PhoneNumberRepo phoneNumberRepo)	{
 		this.cardRepo = cardRepo;
+		this.addressRepo = addressRepo;
+		this.phoneNumberRepo = phoneNumberRepo;
 		
-		cardRepo.save(new Card("Steve1", "Jones", "The Boss", "Liberty Mutual"));
-		cardRepo.save(new Card("Steve2", "Jones", "The Boss", "Liberty Mutual"));
-		cardRepo.save(new Card("Steve3", "Jones", "The Boss", "Liberty Mutual"));
-		cardRepo.save(new Card("Steve4", "Jones", "The Boss", "Liberty Mutual"));
-		cardRepo.save(new Card("Steve5", "Jones", "The Boss", "Liberty Mutual"));
+		Card card1 = new Card("Steve1", "Jones", "The Boss", "Liberty Mutual");
+		Card card2 = new Card("Steve2", "Jones", "The Boss", "Liberty Mutual");
+		Card card3 = new Card("Steve3", "Jones", "The Boss", "Liberty Mutual");
+		cardRepo.save(card1);
+		cardRepo.save(card2);
+		cardRepo.save(card3);
+		Address address1 = new Address("Home", "123 Fake St.", "Fakesville", "DE", 12345, card1);
+		Address address2 = new Address("Home", "456 Fake St.", "Fakesville", "DE", 12345, card2);
+		Address address3 = new Address("Home", "567 Fake St.", "Fakesville", "DE", 12345, card3);
+		addressRepo.save(address1);
+		addressRepo.save(address2);
+		addressRepo.save(address3);
+		PhoneNumber phoneNumber1 = new PhoneNumber("Work", 1234567L, card1);
+		PhoneNumber phoneNumber2 = new PhoneNumber("Work", 9876543210L, card2);
+		PhoneNumber phoneNumber3 = new PhoneNumber("Work", 18888675309L, card3);
+		phoneNumberRepo.save(phoneNumber1);
+		phoneNumberRepo.save(phoneNumber2);
+		phoneNumberRepo.save(phoneNumber3);
 	}
 	
 	@GetMapping("")
@@ -65,5 +87,27 @@ public class CardApiController {
 		card.setId(id);
 		return cardRepo.save(card);
 	}
+	
+	
+	@PostMapping("{id}/addresses")
+	public Card createAddressForCard(@PathVariable long id, @RequestBody Address address)	{
+		Card card = cardRepo.findOne(id);
+					
+		addressRepo.save(address);
+		return card;
+	}
+
+	@PostMapping("{id}/phones")
+	public Card createPhoneNumberForCard(@PathVariable long id, @RequestBody PhoneNumber phoneNumber)	{
+		Card card = cardRepo.findOne(id);
+		phoneNumber = phoneNumberRepo.findOne(phoneNumber.getId());
+		
+			
+		phoneNumberRepo.save(phoneNumber);
+		return card;
+	}
+	
+	
+	
 
 }
